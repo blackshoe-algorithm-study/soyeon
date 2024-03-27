887c3cd44a453a12ffbe078278f8965f4c408293
Kong-E <kng001016@dgu.ac.kr>
2024-03-27 10:29:53 +0900

    최대힙

---
+++ week2_0327/최대힙.py
@@ -0,0 +1,18 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+q = []
+
+for _ in range(n):
+    num = int(input())
+
+    if num == 0:
+        if len(q) > 0:
+            print(-heapq.heappop(q))
+        else:
+            print(0)
+    if num > 0:
+        heapq.heappush(q, -num)
