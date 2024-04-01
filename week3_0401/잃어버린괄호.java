c347100e001827020ed3717489d353a1ec86863a
Kong-E <kng001016@dgu.ac.kr>
2024-04-01 22:05:19 +0900

    잃어버린 괄호

--- week3_0401/잃어버린괄호.java
+++ week3_0401/잃어버린괄호.java
@@ -6,6 +6,23 @@ import java.util.*;
 public class 잃어버린괄호 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+        String[] subtract = br.readLine().split("-");
+
+        int sum = Integer.MAX_VALUE;
+
+        for (int i=0; i<subtract.length; i++) {
+            int temp = 0;
+
+            String[] plus = subtract[i].split("\\+");
+
+            for (String num : plus) {
+                temp += Integer.parseInt(num);
+            }
+
+            if (sum == Integer.MAX_VALUE) sum = temp;
+            else sum -=temp;
+        }
+
+        System.out.println(sum);
     }
 }
