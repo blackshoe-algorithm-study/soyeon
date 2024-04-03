3fe88b601ebf82978ebc36307d6d8e66437b8a33
Kong-E <kng001016@dgu.ac.kr>
2024-04-03 20:45:41 +0900

    진법변환 수정

--- week3_0403/진법변환.java
+++ week3_0403/진법변환.java
@@ -5,12 +5,9 @@ import java.util.*;
 
 public class 진법변환 {
     public static void main(String[] args) throws IOException {
-        // sum(수*B^(자릿수))
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         Map<Character, Integer> map = new HashMap<>();
-        Character[] numCharArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-        List<Character> numCharList = new ArrayList<>(Arrays.asList(numCharArray));
 
         char current = 'A';
 
@@ -24,8 +21,8 @@ public class 진법변환 {
 
         int sum = 0;
          for (int i=0; i<N.length(); i++) {
-             if (numCharList.contains(N.charAt(i))) {
-                 sum += Integer.parseInt(String.valueOf(N.charAt(i))) * Math.pow(B, N.length()-i-1);
+             if (Character.isDigit(N.charAt(i))) {
+                 sum += (N.charAt(i) - '0') * Math.pow(B, N.length() - i - 1);
              } else {
                  sum += map.get(N.charAt(i)) * Math.pow(B, N.length() - i - 1);
              }
