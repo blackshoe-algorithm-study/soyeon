a0dd9de83ad6c986a149bfda34c5f1f009d59c78
Kong-E <kng001016@dgu.ac.kr>
2024-04-04 15:27:29 +0900

    수찾기: 이분탐색 라이브러리

--- week3_0403/수찾기.java
+++ week3_0403/수찾기.java
@@ -7,20 +7,23 @@ public class 수찾기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] n_list = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i=0; i<n; i++) {
             int num = Integer.parseInt(st.nextToken());
-            map.put(num, 0);
+            n_list[i] = num;
         }
+
+        Arrays.sort(n_list);
 
         int m = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
         for (int i=0; i<m; i++) {
             int num = Integer.parseInt(st.nextToken());
-            if (map.containsKey(num)) System.out.println(1);
+            int result = Arrays.binarySearch(n_list, num);
+            if (result >= 0) System.out.println(1);
             else System.out.println(0);
         }
     }
