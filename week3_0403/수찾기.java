f51d8ee0191550f1a28217811d03c57b20a779d7
Kong-E <kng001016@dgu.ac.kr>
2024-04-04 15:22:06 +0900

    수찾기: HashMap

--- week3_0403/수찾기.java
+++ week3_0403/수찾기.java
@@ -1,8 +1,27 @@
 package week3_0403;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class 수찾기 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, 0);
+        }
+
+        int m = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (map.containsKey(num)) System.out.println(1);
+            else System.out.println(0);
+        }
     }
 }
