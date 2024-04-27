f4e438d4705463fc3cc3c34320710504791608e8
Kong-E <kng001016@dgu.ac.kr>
2024-04-27 21:59:34 +0900

    내리막길: dfs only (재귀)
    시간초과

--- week6_0424/내리막길.java
+++ week6_0424/내리막길.java
@@ -4,19 +4,24 @@ import java.io.*;
 import java.util.*;
 
 public class 내리막길 {
+    static int m;
+    static int n;
+    static int[][] map;
+    static boolean[][] visited;
+    static int cnt = 0;
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {-1, 1, 0, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
+        StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
 
-        int[][] map = new int[m][n];
-        boolean[][] visited = new boolean[m][n];
+        map = new int[m][n];
+        visited = new boolean[m][n];
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
@@ -24,37 +29,28 @@ public class 내리막길 {
                 map[i][j] = Integer.parseInt(st.nextToken());
         }
 
-        Stack<int[]> stack = new Stack<>();
-        stack.push(new int[] { 0, 0 });
-
-        int cnt = 0;
-        while (!stack.isEmpty()) {
-            int[] curr = stack.pop();
-            int x = curr[0];
-            int y = curr[1];
-
-            if (x == n - 1 && y == m - 1) {
-                cnt++;
-                for (boolean[] row : visited)
-                    Arrays.fill(row, false);
-                continue;
-            }
-
-            if (!visited[y][x]) {
-                visited[y][x] = true;
-                for (int i = 0; i < 4; i++) {
-                    int nx = x + dx[i];
-                    int ny = y + dy[i];
-
-                    if (nx >= n || ny >= m || nx < 0 || ny < 0) continue;
-
-                    if (map[y][x] > map[ny][nx]) {
-                        stack.push(new int[] { nx, ny });
-                    }
-                }
-            }
-        }
+        dfs(0, 0);
 
         System.out.print(cnt);
+    }
+
+    static void dfs(int x, int y) {
+        if (x == n - 1 && y == m - 1) {
+            cnt++;
+            for (boolean[] row : visited)
+                Arrays.fill(row, false);
+            return;
+        }
+
+        visited[y][x] = true;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= n || ny >= m || nx < 0 || ny < 0) continue;
+
+            if (map[y][x] > map[ny][nx])
+                dfs(nx, ny);
+        }
     }
 }
