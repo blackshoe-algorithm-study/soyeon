f509a013c5a45a4b4663d95ea642d6501fb9dc6c
Kong-E <kng001016@dgu.ac.kr>
2024-06-27 16:27:41 +0900

    week12_0605 오목

--- week12_0605/오목.java
+++ week12_0605/오목.java
@@ -4,6 +4,9 @@ import java.io.*;
 import java.util.*;
 
 public class 오목 {
+    static int[] dx = {1, 1, 1, 0};
+    static int[] dy = {0, 1, -1, 1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -17,73 +20,52 @@ public class 오목 {
             }
         }
 
-        boolean found = false;
-        for (int i = 0; i < 15; i++) {
-            for (int j = 0; j < 15; j++) {
-                int current = board[i][j];
-                if (current != 0) {
-                    if (find(board, i, j, current)) {
-                        System.out.println(current);
-                        System.out.print(i+1 + " ");
-                        System.out.print(j+1);
-                        found = true;
-                        break;
-                    }
-                }
-            }
-            if (found) break;
-        }
+        boolean found = find(board);
 
         if (!found) System.out.println(0);
     }
 
-    static boolean find(int[][] board, int row, int col, int target) {
-        // 아래 방향 검사
-        if (
-                inRange(row + 1, row + 2, row + 3, row + 4, row + 5, row + 6) &&
-                board[row + 1][col] == target && board[row + 2][col] == target &&
-                board[row + 3][col] == target && board[row + 4][col] == target &&
-                (board[row + 5][col] != target || (board[row + 5][col] == target && board[row + 6][col] == target))
-        ) {
-            return true;
-        }
+    static boolean find(int[][] board) {
+        for (int y = 0; y < 19; y++) {
+            for (int x = 0; x < 19; x++) {
+                int current = board[y][x];
+                if (current != 0) {
+                    for (int i = 0; i < 4; i++) { // 4가지 방향
+                        int nx = x;
+                        int ny = y;
+                        int count = 1;
 
-        // 오른쪽 방향 검사
-        if (
-                inRange(col + 1, col + 2, col + 3, col + 4, col + 5, col + 6) &&
-                board[row][col + 1] == target && board[row][col + 2] == target &&
-                board[row][col + 3] == target && board[row][col + 4] == target &&
-                (board[row][col + 5] != target || (board[row][col + 5] == target && board[row][col + 6] == target))
-        ) {
-            return true;
-        }
+                        while (true) {
+                            nx += dx[i];
+                            ny += dy[i];
 
-        // 오른쪽 대각선 방향 검사
-        if (
-                inRange(row + 1, row + 2, row + 3, row + 4, row + 5, row + 6) &&
-                inRange(col + 1, col + 2, col + 3, col + 4, col + 5, col + 6) &&
-                board[row + 1][col + 1] == target && board[row + 2][col + 2] == target &&
-                board[row + 3][col + 3] == target && board[row + 4][col + 4] == target &&
-                (board[row + 5][col + 5] != target || (board[row + 5][col + 5] == target && board[row + 6][col + 6] == target))
-        ) {
-            return true;
-        }
+                            if (nx < 0 || nx >= 19 || ny < 0 || ny >= 19 || board[ny][nx] != current) {
+                                break;
+                            }
 
-        // 왼쪽 대각선 방향 검사
-        if (
-                inRange(row - 1, row - 2, row - 3, row - 4, row - 5, row - 6) &&
-                inRange(col - 1, col - 2, col - 3, col- 4, col - 5, col -6) &&
-                board[row - 1][col - 1] == target && board[row - 2][col - 2] == target &&
-                board[row - 3][col - 3] == target && board[row - 4][col - 4] == target &&
-                (board[row - 5][col - 5] != target || (board[row - 5][col - 5] == target && board[row - 6][col - 6] == target))
-        ) {
-            return true;
-        }
+                            count++;
 
+                            if (count == 5) {
+                                int isBackSixX = nx + dx[i];
+                                int isBackSixY = ny + dy[i];
+                                if (isBackSixY >= 0 && isBackSixY < 19 && isBackSixX >= 0 && isBackSixX < 19 && board[isBackSixY][isBackSixX] == current) {
+                                    break;
+                                }
+                                int isFrontSixX = x - dx[i];
+                                int isFrontSixY = y - dy[i];
+                                if (isFrontSixX >= 0 && isFrontSixX < 19 && isFrontSixY >= 0 && isFrontSixY < 19 && board[isFrontSixY][isFrontSixX] == current) {
+                                    break;
+                                }
+                                System.out.println(current);
+                                System.out.print((y + 1) + " ");
+                                System.out.print((x + 1));
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
         return false;
-    }
-
-    static boolean inRange(int a, int b, int c, int d, int e, int f) {
-        return a < 19 && b < 19 && c < 19 && d < 19 && e < 19 && f < 19 && a >= 0 && b >= 0 && c >= 0 && d >= 0 && e >= 0 && f >= 0;
     }
 }
