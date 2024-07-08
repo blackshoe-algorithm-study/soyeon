549c5e224056b06c0e23d0898b77029eb1d02e33
Kong-E <kng001016@dgu.ac.kr>
2024-07-08 21:46:07 +0900

    week15_0707 팰린드롬?

--- week15_0707/팰린드롬물음표.java
+++ week15_0707/팰린드롬물음표.java
@@ -1,13 +1,53 @@
 package week15_0707;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class 팰린드롬물음표 {
     public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
+        int[] seq = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int M = Integer.parseInt(br.readLine());
+
+        boolean[][] dp = new boolean[N + 1][N + 1];
+
+        // i번째 수부터 i번째 수일 경우
+        for (int i = 1; i <= N; i++) {
+            dp[i][i] = true;
+        }
+        // i번째 수부터 i + 1번째 수일 경우
+        for (int i = 1; i <= N - 1; i++) {
+            if (seq[i] == seq[i + 1]) {
+                dp[i][i + 1] = true;
+            } else {
+                dp[i][i + 1] = false;
+            }
+        }
+        // i번째 수부터 j(j >= i + 2)번째 수일 경우
+        for (int len = 2; len <= N - 1; len++) {
+            for (int i = 1; i <= N - len; i++) {
+                if (seq[i] == seq[i + len]) {
+                    dp[i][i + len] = dp[i + 1][i + len - 1];
+                }
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            boolean answer = dp[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())];
+            if (answer) sb.append(1).append("\n");
+            else sb.append(0).append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
